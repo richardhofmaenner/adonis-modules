bd804ab74001e6137e5319b943e27ff4e6721f28
Richard Hofmänner <hello@terillos.dev>
2020-11-22 13:46:00 +0100

    Added Module Model and migration

---
+++ app/Models/Module.ts
@@ -0,0 +1,25 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Module extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public logoUrl: string
+
+  @column()
+  public description: string
+
+  @column()
+  public packageUrl: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
