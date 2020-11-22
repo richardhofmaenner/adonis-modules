f4ef1a7930f080cd6bd02cd758b17f067505fdbd
Richard Hofmänner <hello@terillos.dev>
2020-11-22 13:46:00 +0100

    Added Module Model and migration

---
+++ database/migrations/1606047399204_modules.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Modules extends BaseSchema {
+  protected tableName = 'modules'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name', 100)
+      table.string('logoUrl', 255)
+      table.text('description')
+      table.string('packageUrl', 255)
+      table.timestamps(true)
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
